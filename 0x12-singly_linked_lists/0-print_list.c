ee658dbc81e66e68ad1c117798b1f15838457f98
Mcnoble <idowufestustemiloluwa@gmail.com>
2023-09-22 07:04:05 -0700

    feat: Print List :octocat:

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,24 @@
 #include "lists.h"
 
 /**
- * print_list - prints all elements of list_t list
- * @h: linked list
- * Return: number of nodes in linked list
+ * print_list - prints list.
+ * @h: param list
+ * Return: struct
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t l_node;
 
+	l_node = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
-		count++;
 		h = h->next;
+		l_node++;
 	}
-
-	return (count);
+	return (l_node);
 }
